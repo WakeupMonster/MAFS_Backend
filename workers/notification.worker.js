12a390d46633b73401487d1e3781d981db21201e
Raj Yadav <raj.yadav29.wm@gmail.com>
2026-01-05 14:17:11 +0530

    update notification.services

--- workers/notification.worker.js
+++ workers/notification.worker.js
@@ -4,7 +4,7 @@
 
 // const worker = new Worker('notification-queue', async (job) => {
 //   const { type, data } = job;
-  
+
 //   console.log(`🚀 Processing ${type} for job ${job.id}`);
 
 //   try {
@@ -28,53 +28,69 @@
 //     console.error(`Worker Error in ${type}:`, error);
 //     throw error; // Isse BullMQ auto-retry karega
 //   }
-// }, { 
+// }, {
 //   connection: redisClient.options,
 //   concurrency: 10 // Ek saath 10 notifications process karega
 // });
 
 // module.exports = worker;
 
-
-const { Worker } = require('bullmq');
+const { Worker } = require("bullmq");
 // const notificationService = require('./notification.service');
-const notificationService = require('../modules/notifications/notification.service');
+const notificationService = require("../modules/notifications/notification.service");
 
 const connection = {
-    host: process.env.REDIS_HOST || '127.0.0.1',
-    port: process.env.REDIS_PORT || 6379
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
 };
 
-const worker = new Worker('notification-queue', async (job) => {
+const worker = new Worker(
+  "notification-queue",
+  async (job) => {
     const { type } = job.name; // Job name as type
     const data = job.data;
 
     console.log(`Processing ${job.name} for Job ID: ${job.id}`);
 
     try {
-        switch (type) {
-            case 'NEW_MATCH':
-                await notificationService.sendNewMatchNotification(data.userId1, data.userId2);
-                break;
-            case 'NEW_MESSAGE':
-                await notificationService.sendNewMessageNotification(data.senderId, data.receiverId, data.messageText);
-                break;
-            case 'NEW_LIKE':
-                await notificationService.sendLikeNotification(data.senderId, data.receiverId);
-                break;
-            case 'GIVEAWAY_WINNER':
-                await notificationService.sendGiveawayWinnerNotification(data.userId, data.prizeTitle);
-                break;
-            case 'PRIZE_DELIVERED':
-                await notificationService.sendPrizeDeliveredNotification(data.userId);
-                break;
-            default:
-                console.warn(`No handler for ${job.name}`);
-        }
+      switch (type) {
+        case "NEW_MATCH":
+          await notificationService.sendNewMatchNotification(
+            data.userId1,
+            data.userId2
+          );
+          break;
+        case "NEW_MESSAGE":
+          await notificationService.sendNewMessageNotification(
+            data.senderId,
+            data.receiverId,
+            data.messageText
+          );
+          break;
+        case "NEW_LIKE":
+          await notificationService.sendLikeNotification(
+            data.senderId,
+            data.receiverId
+          );
+          break;
+        case "GIVEAWAY_WINNER":
+          await notificationService.sendGiveawayWinnerNotification(
+            data.userId,
+            data.prizeTitle
+          );
+          break;
+        case "PRIZE_DELIVERED":
+          await notificationService.sendPrizeDeliveredNotification(data.userId);
+          break;
+        default:
+          console.warn(`No handler for ${job.name}`);
+      }
     } catch (error) {
-        console.error(`Worker Error: ${error.message}`);
-        throw error;
+      console.error(`Worker Error: ${error.message}`);
+      throw error;
     }
-}, { connection, concurrency: 10 });
+  },
+  { connection, concurrency: 10 }
+);
 
 module.exports = worker;
