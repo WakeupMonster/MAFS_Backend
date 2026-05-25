5e8326fffe1cd805b3bbe9a154505487e0f0e195
Raj Yadav <raj.yadav29.wm@gmail.com>
2026-05-25 14:02:37 +0530

    Pull from the dev branch 25-May and update code.

--- modules/Admin/adminNotificationCampaigns/adminNotification.routes.js
+++ modules/Admin/adminNotificationCampaigns/adminNotification.routes.js
@@ -10,7 +10,10 @@ const {
   sendIndividualNotification,
 } = require("./adminNotification.controller");
 
-const { createEmailCampaign, getEmailCampaignLogs } = require("./adminEmail.controller");
+const {
+  createEmailCampaign,
+  getEmailCampaignLogs,
+} = require("./adminEmail.controller");
 
 router.get("/email-campaign/:campaignId/logs", getEmailCampaignLogs);
 
@@ -20,6 +23,7 @@ router.post("/broadcastemail", createEmailCampaign);
 
 router.post("/premium/send", sendNotificationToPremiumUsers);
 
+// compare this one and
 router.post("/individual", sendIndividualNotification);
 
 router.post("/premium-expiry/send", createPremiumExpiryCampaign);
@@ -28,6 +32,7 @@ router.post("/premium-expiry/:campaignId/trigger", sendPremiumExpiryNow);
 
 router.get("/notifications/history", getNotificationHistory);
 
+// this one
 router.post("/send-single", sendSingleUserNotification);
 
 router.patch("/update/:userId", updateNotificationSettings);
