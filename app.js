da9cae554ad0f66c890ac5dcda243eb617acf7b9
Raj Yadav <raj.yadav29.wm@gmail.com>
2026-08-18 16:17:15 +0530

    Update code

--- app.js
+++ app.js
@@ -43,15 +43,17 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // ─── CORS ───
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL
-      ? process.env.FRONTEND_URL.split(",").map(s => s.trim())
-      : "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://127.0.0.1:5500"
+//       ? process.env.FRONTEND_URL.split(",").map(s => s.trim())
+//       : "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   }),
+// );
+
+app.use(cors({ origin: '*' })); // dev ke liye
 
 // ─── Request Logging (Development Only) ───
 if (
